809eceb7755f490c6ada3b7dee2b6b86c8babde4
Josh Holman <git@jolman.me>
2025-11-05 16:16:18 -0800

    command line args thing

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
+    if env::args().len() > 1 {
+        eprintln!(
+            "Error: {} exptects no arguments",
+            env::args().next().unwrap()
+        );
+        std::process::exit(1);
+    }
+
     println!("Hello, world!");
 }
